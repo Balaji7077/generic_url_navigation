1a5956f990b33d2047482bbb6daf0425de11e91f
Balaji7077 <Balajisabat6@gmail.com>
2023-11-21 23:16:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def balaji(request):
+    return render(request,'balaji.html')
+def roshni(request):
+    return render(request,'roshni.html')
+def balaji2(request):
+    return render(request,'balaji2.html')
